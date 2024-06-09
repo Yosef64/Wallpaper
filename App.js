0793f982511c7b9f740efd20b24ca5eb4abcb0b7
johnrich55 <yohanneshabtamu524@gmail.com>
2024-06-09 12:25:33 -0700

    test pull request

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View,StyleSheet } from "react-native";
 import { useFonts } from "expo-font";
 // import AppLoading from 'expo-app-loading';
 import Appbar from "./components/Appbar/Appbar";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import Body from "./components/mainBody/Body";
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <View style={{ flex: 1 }}>
+        <View style={styles.container}>
           <Appbar />
+          <Body/>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgb(10, 10, 34)",
+   
+  },
+}
+)
